3ca4275055c306a58530ee716e17507060014b21
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2025-09-27 16:17:34 -0400

    symbols: Add documentation

--- ghidra/scripts/M68kMacSymbols.java
+++ ghidra/scripts/M68kMacSymbols.java
@@ -35,12 +35,26 @@ public class M68kMacSymbols extends GhidraScript {
                         int length = getByte(symbolAddr) & 0xff;
                         symbolAddr = symbolAddr.addNoWrap(1);
                         if (length == 0x80) {
+                            // With a variable-length format, the first byte is in the range $80 to $9F.
+                            // Stripping the high-order bit produces a length in the range $00 through $1F.
+                            // If the length is zero, the next byte contains the actual length, in the range
+                            // $01 through $FF.
+                            //
+                            // So since we added one to the symbol address already, we are getting the NEXT byte.
+                            // So this is the length byte right here, and we want to read past it.
                             length = getByte(symbolAddr) & 0xff;
                             symbolAddr = symbolAddr.addNoWrap(1);
                         } else if (length > 0x80) {
                             length -= 0x80;
                         } else {
                             // TODO: 16 byte fixed length symbols
+                            // With fixed-length format, the first byte is in the range $20 through $7F.
+                            // The high-order bit may or may not be set. The high-order bit of the second
+                            // byte is set for 16-character names, clear for 8-character names. Fixed-length
+                            // 16-character names are used in object Pascal to show class.method names instead
+                            // of procedure names. The method name is contained in the first 8 bytes and the
+                            // class name is in the second 8 bytes. MacsBug swaps the order and inserts the period
+                            // before displaying the name.
                             length = 8;
                             symbolAddr = symbolAddr.addNoWrap(-1);
                         }
